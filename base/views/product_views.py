3fe11dae816ec86db5a8abeb72031e5ccd3a74c8
carsonep <carsonepeterson@gmail.com>
2021-03-30 16:38:53 -0500

    added feature to upload products as a admin

--- base/views/product_views.py
+++ base/views/product_views.py
@@ -21,3 +21,62 @@ def getProduct(request, pk):
     serializer = BeatSerializer(beat, many=False)
 
     return Response(serializer.data)
+
+@api_view(['POST'])
+@permission_classes([IsAdminUser])
+def createProduct(request):
+    user = request.user
+    beat = Beat.objects.create(
+        user=user,
+        name="Sample Beat",
+        price=0,
+        scale="C major",
+        bpm="100 BPM",
+        tags='#Sample',
+    )
+
+    serializer = BeatSerializer(beat, many=False)
+
+    return Response(serializer.data)
+
+@api_view(['PUT'])
+@permission_classes([IsAdminUser])
+def updateProduct(request, pk):
+    data = request.data
+    beat = Beat.objects.get(_id=pk)
+
+    print(data)
+
+    beat.name = data['name']
+    beat.price = data['price']
+    beat.scale = data['scale']
+    beat.bpm = data['bpm']
+    beat.tags = data['tags']
+
+    beat.save()
+    serializer = BeatSerializer(beat, many=False)
+    return Response('Product updated')
+
+@api_view(['POST'])
+def uploadBeat(request):
+    data = request.data
+
+    beat_id = data['beat_id']
+    beat = Beat.objects.get(_id=beat_id)
+
+    beat.beat = request.FILES.get('beat')
+    beat.save()
+    return Response('Beat was uploaded')
+
+@api_view(['DELETE'])
+@permission_classes([IsAdminUser])
+def deleteProduct(request, pk):
+    beat = Beat.objects.get(_id=pk)
+    beat.delete()
+
+    return Response("Beat Deleted")
+
+
+
+
+
